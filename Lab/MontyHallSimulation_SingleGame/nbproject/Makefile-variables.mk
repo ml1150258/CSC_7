f7aa1408b28fa7b24f3bc32f7fb61643a619275b
Lehr <mark.lehr@rcc.edu>
2023-12-11 10:58:16 -0800

    Signed-off-by: Lehr <mark.lehr@rcc.edu>

--- Lab/MontyHallSimulation_SingleGame/nbproject/Makefile-variables.mk
+++ Lab/MontyHallSimulation_SingleGame/nbproject/Makefile-variables.mk
@@ -7,21 +7,21 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=MinGW-Windows
-CND_ARTIFACT_DIR_Debug=dist/Debug/MinGW-Windows
+CND_PLATFORM_Debug=Cygwin-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
 CND_ARTIFACT_NAME_Debug=montyhallsimulation_singlegame
-CND_ARTIFACT_PATH_Debug=dist/Debug/MinGW-Windows/montyhallsimulation_singlegame
-CND_PACKAGE_DIR_Debug=dist/Debug/MinGW-Windows/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/montyhallsimulation_singlegame
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
 CND_PACKAGE_NAME_Debug=montyhallsimulationsinglegame.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/MinGW-Windows/package/montyhallsimulationsinglegame.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/montyhallsimulationsinglegame.tar
 # Release configuration
-CND_PLATFORM_Release=MinGW-Windows
-CND_ARTIFACT_DIR_Release=dist/Release/MinGW-Windows
+CND_PLATFORM_Release=Cygwin-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
 CND_ARTIFACT_NAME_Release=montyhallsimulation_singlegame
-CND_ARTIFACT_PATH_Release=dist/Release/MinGW-Windows/montyhallsimulation_singlegame
-CND_PACKAGE_DIR_Release=dist/Release/MinGW-Windows/package
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/montyhallsimulation_singlegame
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
 CND_PACKAGE_NAME_Release=montyhallsimulationsinglegame.tar
-CND_PACKAGE_PATH_Release=dist/Release/MinGW-Windows/package/montyhallsimulationsinglegame.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/montyhallsimulationsinglegame.tar
 #
 # include compiler specific variables
 #
